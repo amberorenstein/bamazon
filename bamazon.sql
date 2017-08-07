a893834a2b6c85118087edabc2b98657d638600a
amberorenstein <amber.orenstein@gmail.com>
2017-08-06 20:25:46 -0600

    basic

---
+++ bamazon.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+CREATE TABLE products(
+  item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(20),
+  department_name VARCHAR(20),
+  price DECIMAL (10,4),
+  stock_quantity INTEGER(11),
+  PRIMARY KEY(item_id)
+);
+
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("banana", "food", 0.25, 3000);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("bicycle", "sports", 590.75, 80);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("headphones", "electronics", 40.00, 302);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("vitamins", "health", 9.99, 3);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("pencils", "office", 0.10, 42000);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("fax machine", "office", 1.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPod", "electronics", 10.00, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("pillow", "home", 50.00, 8764);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gum", "food", 0.25, 53050);
+
+USE bamazon_db;
+
+SELECT * FROM products;
+
+DELETE FROM products
+WHERE item_id = 11;
+DELETE FROM products
+WHERE item_id = 12;
+DELETE FROM products
+WHERE item_id = 13;
+DELETE FROM products
+WHERE item_id = 14;
+DELETE FROM products
+WHERE item_id = 15;
+DELETE FROM products
+WHERE item_id = 16;
+DELETE FROM products
+WHERE item_id = 17;
+DELETE FROM products
+WHERE item_id = 18;
+
+
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,75 @@
+//dependencies
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  // Your username
+  user: "root",
+  // Your password
+  password: "",
+  database: "bamazon_db"
+});
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected");
+});
+
+var run = function()
+{connection.query("SELECT * FROM products", function(err,results){
+  for(let i = 0; i<results.length; i++) {
+    console.log("item id: " + results[i].item_id + " | " + results[i].product_name + " | " + results[i].department_name + " | " + results[i].price + " | " + results[i].stock_quantity + "\n");
+  }
+  inquirer
+  .prompt([{
+    name: "itemId",
+    type: "input",
+    message: "What is the item ID you would like to buy?",
+    validate: function(value) {
+      if (isNaN(value) == false) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }, {
+    name: "Quantity",
+    type: "input",
+    message: "How many of this item would you like to buy?",
+    validate: function(value) {
+      if (isNaN(value) === false) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }]).then(function(answer) {
+    var chosenId = answer.itemId - 4;
+    var chosenProduct = results[chosenId];
+    var chosenQuantity = answer.Quantity;
+    var inStock = results[chosenId].stock_quantity;
+    if (chosenQuantity < inStock) {
+      console.log("Your total for " + "(" + chosenQuantity + ")" + " - " + results[chosenId].product_name + " is: " + results[chosenId].price.toFixed(2) * chosenQuantity);
+      connection.query("UPDATE products SET ? WHERE ?",
+      [
+        {
+        stock_quantity: (inStock - chosenQuantity)
+      }, {
+        item_id: chosenId
+      }], function(err, results) {
+        run();
+      });
+    } else {
+      console.log("Sorry, insufficient Quanity at this time. All we have is " + inStock + " in our Inventory.");
+      run();
+    }
+}).catch(function(err){
+  console.log(err);
+});
+});
+}
+
+run();
